6fa1ac84e544eb4ae42e5fa7adea05e99e020793
NizamChy <nizam.chy.cs@gmail.com>
2025-07-17 18:22:41 +0600

    responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
-      <div className="fixed z-20 bg-white w-full">
-        <nav className="min-h-16 flex border justify-center items-center px-4 lg:px-8 relative">
-          <ul className="text-base text-gray-600 font-semibold flex gap-4 text-center">
-            <li className="border p-2">Add Bill</li>
-            <li className="border p-2">Product Info</li>
-            <li className="border p-2">Business Info</li>
+      <div className="fixed z-20 bg-white w-full my-2">
+        <nav className="min-h-16 flex border justify-center items-center relative">
+          <ul className="text-sm sm:text-base text-gray-600 sm:font-semibold flex gap-2 sm:gap-4 text-center">
+            <li className="border p-2 text-nowrap">Add Bill</li>
+            <li className="border p-2 text-nowrap">Product Info</li>
+            <li className="border p-2 text-nowrap">Business Info</li>
           </ul>
         </nav>
       </div>
