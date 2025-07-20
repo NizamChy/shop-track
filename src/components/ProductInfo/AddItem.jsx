e23638f751f75e09edfaa5fe2deced40d15e8942
NizamChy <nizam.chy.cs@gmail.com>
2025-07-20 18:14:31 +0600

    business route

--- src/components/ProductInfo/AddItem.jsx
+++ src/components/ProductInfo/AddItem.jsx
@@ -60,7 +60,7 @@ const AddItem = () => {
               </label>
               <input
                 type="text"
-                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-teal-500"
+                className="w-full px-3 py-2 border border-gray-300 bg-slate-50 rounded-md focus:outline-none focus:ring-1 focus:ring-teal-500"
                 placeholder="Item name"
               />
             </div>
@@ -71,7 +71,7 @@ const AddItem = () => {
               </label>
               <input
                 type="text"
-                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-teal-500"
+                className="w-full px-3 py-2 border border-gray-300 bg-slate-50 rounded-md focus:outline-none focus:ring-1 focus:ring-teal-500"
                 placeholder="৳"
                 step="0.01"
               />
