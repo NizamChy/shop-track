855ba9e674a8abcc009ec9547251be0a7efff568
NizamChy <nizam.chy.cs@gmail.com>
2025-07-23 18:23:31 +0600

    calendar updated

--- src/components/BusinessInfo/BusinessInfo.jsx
+++ src/components/BusinessInfo/BusinessInfo.jsx
@@ -3,10 +3,13 @@
 import Calendar from "react-calendar";
 import React, { useState } from "react";
 import "react-calendar/dist/Calendar.css";
+import { LuCalendarDays } from "react-icons/lu";
 import { useOrderHistory } from "@/context/OrderHistoryContext";
 
 const BusinessInfo = () => {
   const [selectedDate, setSelectedDate] = useState(new Date());
+  const [showCalendar, setShowCalendar] = useState(false);
+
   const { getOrdersByDate } = useOrderHistory();
 
   const dailyOrders = getOrdersByDate(selectedDate);
@@ -16,13 +19,45 @@ const BusinessInfo = () => {
     <div className="p-4">
       <h1 className="text-2xl font-bold mb-4">Sales Overview</h1>
 
-      <div className="mb-6">
+      <button
+        className="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-4"
+        onClick={() => setShowCalendar(!showCalendar)}
+      >
+        <span className="flex items-center justify-center gap-2">
+          <LuCalendarDays className="text-xl" /> Select Date
+        </span>
+      </button>
+
+      {showCalendar && (
+        <div className="mb-6 mt-2 transition-all duration-300 ease-in-out">
+          <Calendar
+            onChange={setSelectedDate}
+            onClickDay={() => setShowCalendar(!showCalendar)}
+            value={selectedDate}
+            className="border rounded p-2"
+          />
+        </div>
+      )}
+
+      {/* <div className="flex justify-evenly items-center">
+      <label htmlFor="todayDate" className="text-nowrap">
+        Select Date:
+      </label>
+      <input
+        type="date"
+        id="todayDate"
+        value={formattedDate}
+        onChange={(e) => setDate(new Date(e.target.value))}
+      />
+    </div> */}
+
+      {/* <div className="mb-6">
         <Calendar
           onChange={setSelectedDate}
           value={selectedDate}
           className="border rounded p-2"
         />
-      </div>
+      </div> */}
 
       <div className="bg-white p-4 rounded shadow">
         <h2 className="text-xl font-semibold mb-2">
